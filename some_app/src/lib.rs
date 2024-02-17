dee1dbb6d526886c480959a5ab40c3f70dadeb58
videobitva <vadimgromovoy@gmail.com>
2024-02-18 00:53:21 +0200

    Add comments

--- some_app/src/lib.rs
+++ some_app/src/lib.rs
@@ -1,11 +1,13 @@
 use leptos::*;
 use some_lib::{associated_type, just_type};
 
-#[server(impl_into = false)]
+// does not compile
+#[server()]
 pub async fn with_assoc_type(data: associated_type::ActiveModel) -> Result<(), ServerFnError> {
     Ok(())
 }
 
+// does compile
 #[server()]
 pub async fn with_just_type(data: just_type::ActiveModel) -> Result<(), ServerFnError> {
     Ok(())
